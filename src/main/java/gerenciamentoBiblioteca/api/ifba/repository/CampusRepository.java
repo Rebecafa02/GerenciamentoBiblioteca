a7a703131d91104584ffc36eda96a20c2d2eae74
Rebeca Almeida <89226655+Rebecafa02@users.noreply.github.com>
2026-04-28 22:29:53 -0300

    Delete src directory

--- src/main/java/gerenciamentoBiblioteca/api/ifba/repository/CampusRepository.java
+++
@@ -1,7 +0,0 @@
-package gerenciamentoBiblioteca.api.ifba.repository;
-
-import gerenciamentoBiblioteca.api.ifba.domain.entity.Campus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampusRepository extends JpaRepository<Campus, Integer> {
-}
